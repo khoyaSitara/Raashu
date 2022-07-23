95b61b776d3207e2e430359ee87fbc67a1ff70c9
DextiNxD <95567528+DextiNxD@users.noreply.github.com>
2022-07-23 18:00:02 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,10 +44,6 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
